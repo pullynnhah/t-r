84fe061acafe2fb09a674af6576e0db3b30c78b0
Paula <pullynnhah@icloud.com>
2023-01-15 04:17:13 -0300

    :bug: slice was fixed previous commit and remove FIXME

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express, { json } from "express";
 import cors from "cors";
+import express, { json } from "express";
 
 const PORT = 5000;
 const USERS_DB = [];
@@ -13,8 +13,7 @@ app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   if (!(username && avatar)) return res.status(400).send("Todos os campos são obrigatórios!");
   if (Object.keys(req.body).length !== 2) return res.sendStatus(400);
-  if (USERS_DB.find(user => user.username === username)) return res.sendStatus(409); // FIXME: change to 400 as was requested
-  USERS_DB.push({ username, avatar });
+  if (USERS_DB.find(user => user.username === username)) return res.sendStatus(400);
   res.status(201).send("OK");
 });
 
